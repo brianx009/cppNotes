2587a98342cf3da9bd71547e176d2dcb7373f334
Brian Saldana <brianxsaldana@gmail.com>
2025-09-22 23:39:31 -0700

    Fixing merge conflicts in section 10. Example

--- 10. Functions in C++: Using Functions & Parameter Passing/10. Passing in Integers as Reference/passingInIntegersAsReferences.cpp
+++ 10. Functions in C++: Using Functions & Parameter Passing/10. Passing in Integers as Reference/passingInIntegersAsReferences.cpp
@@ -1,3 +1,32 @@
 #include <iostream>
+#include <string>
 
-using std::
+using std::cout;
+using std::endl;
+
+//Example 1: Example is made to show how c++ makes certain conversions that might not be what we want. For example, we are passing an int into the printValue function that is supposed to accept a double as a parameter. We could assume that this is possible because there is something under the hood that creates a temp variable that holds a refernce of the converted integer to a double. Conversion from int --> const double&. Alternatively, we see that when we try using squareValue, this does not work as we are attempting to go from a int --> double&. This gives us insight as to what c++ considers okay to convert, it only accepts it if it is going from int --> const double&
+
+void printValue(const double& num);
+double& squareValue(double& num);     //Return type is of a reference to a double. Input param is also a reference.
+
+int main(){
+
+  int intVar = 555;
+
+  printValue(intVar);     // here we will see the conversion occur because it is a conversion from int --> const double&
+
+  double result = squareValue(intVar);     // this will not work and we get an error. Conversion from int --> double&
+  cout << result << endl;
+  
+  return 0;
+}
+
+void printValue(const double& num){
+  cout << "___Number is: " << num << "___" << endl;
+}
+
+double& squareValue(double& num){
+  num *= num;
+  
+  return num;
+}
--- 10. Functions in C++: Using Functions & Parameter Passing/9. Passing Vectors and Strings Using References/passingVectorsAndStringsUsingReferences.cpp
+++ 10. Functions in C++: Using Functions & Parameter Passing/9. Passing Vectors and Strings Using References/passingVectorsAndStringsUsingReferences.cpp
@@ -49,23 +49,93 @@ using std::vector;
 //   // }
 // }
 
-//Example 2:
 
-void formatAndPrint(string& firstName, string& lastName, string& city);
+//Example 2: This just shows how we could print out strings -- nothing too impressive
+// void formatAndPrint(string& firstName, string& lastName, string& city);
+
+// int main(){
+
+//   string fName = "Brian";
+//   string lName = "Saldana";
+//   string city = "Brian's City";
+
+//   formatAndPrint(fName, lName, city);
+  
+//   return 0;
+// }
+
+// void formatAndPrint(string& firstName, string& lastName, string& city){
+
+//   string formattedString = "*First Name: " + firstName + "*\n" +     // to line up strings perfectly you can use setw from #include <iomanip>
+// 	"*Last Name: " + lastName + "*\n" + "*City: " + city + "*\n";
+
+//   cout << formattedString << endl;;
+  
+// }
+
+//Example 3: Building on example 2, we show that when we try to pass in string literals into the function directly, we get an error message when attempting to compile -- why is this ? This is because our function is attempting to take a reference of something that is being passed in as a literal -- it will however work if we change the functions reference parameters to const reference parameters -- this is because by definition a string literal should be immutable.
+// void formatAndPrint(string& firstName, string& lastName, string& city);
+
+// int main(){
+
+//   // string fName = "Brian";
+//   // string lName = "Saldana";
+//   // string city = "Brian's City";
+
+//   formatAndPrint("Brian", "Saldana", "Brian's City");
+  
+//   return 0;
+// }
+
+// void formatAndPrint(string& firstName, string& lastName, string& city){
+
+//   string formattedString = "*First Name: " + firstName + "*\n" +     // to line up strings perfectly you can use setw from #include <iomanip>
+// 	"*Last Name: " + lastName + "*\n" + "*City: " + city + "*\n";
+
+//   cout << formattedString << endl;;
+  
+// }
+
+//Example 4: Changing reference string to const reference string in function -- and despite us having the desired output to the console, we have to note that under the hood we might be having a behavior that is not wanted. What do we mean by that? -- because we are passing in string literals, c++ has to create a temporary variable with the strig literal values that we are passing in,then it is making a const reference to that temporary string object that was created. The whole point of wanting to use "const references" is so we dont make copies or create anything extra, but here we are creating extra temp objects.
+// void formatAndPrint(const string& firstName, const string& lastName, const string& city);
+
+// int main(){
+
+//   // string fName = "Brian";
+//   // string lName = "Saldana";
+//   // string city = "Brian's City";
+
+//   formatAndPrint("Brian", "Saldana", "Brian's City");
+  
+//   return 0;
+// }
+
+// void formatAndPrint(const string& firstName, const string& lastName, const string& city){
+
+//   string formattedString = "*First Name: " + firstName + "*\n" +     // to line up strings perfectly you can use setw from #include <iomanip>
+// 	"*Last Name: " + lastName + "*\n" + "*City: " + city + "*\n";
+
+//   cout << formattedString << endl;;
+  
+// }
+
+//Example 5: Building off of Example 4, there is a solution C++ 17 has implemented that helps us not create temporary objects -- std::string_view -- this is a new type that is exactly identical to a string, except it is entirely const ! -- keep note that although it is very similar to a string, it is missing some overloaded operators (we can not use the + operator to concatonate strings). So the work around is to stream it directly to cout with <<.
+void formatAndPrint(std::string_view firstName, std::string_view lastName, std::string_view city);
 
 int main(){
+  // string fName = "Brian";
+  // string lName = "Saldana";
+  // string city = "Brian's City";
 
-  string firstName = "Brian";
-  string lastName = "Last Name";
-  string city = "City";
-
-  formatAndPrint(firstName, lastName, city);
-
+  formatAndPrint("Brian", "Saldana", "Brian's City");
+  
   return 0;
 }
 
-void formatAndPrint(string& firstName, string& lastName, string& city){
-  string formattedString = "Formatted String: " + firstName + ", " + lastName + ", " + city;
+void formatAndPrint(std::string_view firstName, std::string_view lastName, std::string_view city){
 
-  std::cout << formattedString << std::endl;
+  cout << "*First Name: " << firstName << "*\n" <<
+	"*Last Name: " << lastName << "*\n" << "*City: " << city << "*\n";
+  
+>>>>>>> Stashed changes
 }
